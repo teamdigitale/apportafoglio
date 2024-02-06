eeb6197343e13a63d24efdc5b96e77db5425bd38
fmancinidtd <143797809+fmancinidtd@users.noreply.github.com>
2024-02-06 18:38:48 +0100

    Update +page.server.js

--- src/routes/accesso/+page.server.js
+++ src/routes/accesso/+page.server.js
@@ -5,38 +5,38 @@ import { CS } from '$env/static/private';
 import { CBURI } from '$env/static/private';
 
 export async function load({ cookies, url, locals }) {
-    
+
     const code = url.searchParams.get('code');
     if (code && code !== '') {
-    const oa = new jsforce.OAuth2({
-        clientId : CID,
-        clientSecret : CS,
-        redirectUri : CBURI
-      });
-    const conn = new jsforce.Connection({ oauth2: oa });
+        const oa = new jsforce.OAuth2({
+            clientId: CID,
+            clientSecret: CS,
+            redirectUri: CBURI
+        });
+        const conn = new jsforce.Connection({ oauth2: oa });
 
 
         await conn.authorize(code);
         const connectionToken = conn.accessToken;
-        if(connectionToken){
-        cookies.set('session_id_std', connectionToken, {
-            path: '/',
-            sameSite: 'strict',
-            secure: true,
-            maxAge: 60 * 60 * 24 * 1
-        });
-    }
+        if (connectionToken) {
+            cookies.set('session_id_std', connectionToken, {
+                path: '/',
+                sameSite: 'strict',
+                secure: true,
+                maxAge: 60 * 60 * 24 * 1
+            });
+        }
         //throw redirect(303, '/accesso');
-    let utentestandard = null;
-    let cookiesfuidstd = null;
-    let loggedstandard = false;
-    let sessionerror = '';
-    let idutentesf = '';
+        let utentestandard = null;
+        let cookiesfuidstd = null;
+        let loggedstandard = false;
+        let sessionerror = '';
+        let idutentesf = '';
         try {
             await conn.identity(function (err, res) {
                 if (err) {
                     sessionerror = err.message;
-                    event.cookies.delete('session_id_std', { path: '/' });
+                    cookies.delete('session_id_std', { path: '/' });
                     loggedstandard = false;
                     cookiesfuidstd = null;
                 } else {
@@ -54,15 +54,15 @@ export async function load({ cookies, url, locals }) {
             utentestandard.idsf = idutentesf;
         } catch (error) {
             sessionerror = error.message;
-            event.cookies.delete('session_id_std', { path: '/' });
+            cookies.delete('session_id_std', { path: '/' });
             loggedstandard = false;
             cookiesfuidstd = null;
             utentestandard = null;
         }
 
-        locals.user.loggedstandard= true;
-        locals.user.utentestandard= utentestandard;
-        throw redirect(301, '/');
+        locals.user.loggedstandard = loggedstandard;
+        locals.user.utentestandard = utentestandard;
+        throw redirect(302, '/accesso');
         /*
         return {
             loggedstandard: true ,
@@ -72,7 +72,7 @@ export async function load({ cookies, url, locals }) {
             refresh: true   
         }*/
     }
-    else{
+    else {
         return {
         }
     }
@@ -100,9 +100,9 @@ export const actions = {
                 secure: true,
                 maxAge: 60 * 60 * 24 * 1
             });
-             
-             
-            
+
+
+
 
         } catch (err) {
             console.log(err.message);
