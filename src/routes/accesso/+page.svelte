737184e9e7276e8d443e416c4c87936f75263222
fmancinidtd <143797809+fmancinidtd@users.noreply.github.com>
2024-02-07 11:32:35 +0100

    Update +page.svelte

--- src/routes/accesso/+page.svelte
+++ src/routes/accesso/+page.svelte
@@ -1,13 +1,17 @@
 <script>
-	import { invalidate } from '$app/navigation';
-	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
 
 	export let data;
 
-	
+
+    
+    onMount(() => {
+        goto('/users',{invalidateAll: true});
+    });
 	
 </script>
 
 <div class="container my-4 text-center">
-logged
+logged in, please wait a moment...
 </div>
