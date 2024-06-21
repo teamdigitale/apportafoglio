a7b666c5431cd0006d26551d2e4ba7895d600f17
fmancinidtd <143797809+fmancinidtd@users.noreply.github.com>
2024-06-21 14:55:00 +0200

    scrtipt action

--- .github/workflows/dist/index.js
+++ .github/workflows/dist/index.js
@@ -1,3 +1,9 @@
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
 const core = require('@actions/core');
 const github = require('@actions/github');
 
