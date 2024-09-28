f3a3a3e7e39ee2924ac86ed024387c3dbcde4d4f
fmancini <fausto.mancini@gmail.com>
2024-09-28 19:09:03 +0200

    modanci

--- src/routes/anci/survey/map/+page.server.js
+++ src/routes/anci/survey/map/+page.server.js
@@ -28,14 +28,14 @@ export async function load({ locals }) {
         let entiStandard = values[0];
 
 
-        const risposte_res = await fetch('https://rollingwords.it/temp/survey_anci.json', {
+        const risposte_res = await fetch('https://rollingwords.it/dist/quest/survey_anci.json', {
             method: 'GET',
             credentials: 'same-origin',
             redirect: 'follow',
             agent: null,
             headers: {
                 "Content-Type": "text/plain",
-                //'Authorization': 'Basic ' + btoa(UNCI+':'+UPCI.replaceAll('##','$')),
+                'Authorization': 'Basic ' + btoa(UNCI+':'+UPCI)
             },
             timeout: 10000
         });
