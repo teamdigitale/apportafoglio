26013b71e3868824a1f7d1d7884c12f6c7390cdb
fmancini <fausto.mancini@gmail.com>
2024-10-27 07:20:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,23 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
 		rollupOptions: {
-		  output: {
-			  entryFileNames: `[name].js?v=${version}`,
-			  chunkFileNames: `[name].js?v=${version}`,
-			  assetFileNames: `[name].[ext]?v=${version}`
-		  }
+			output: {
+				entryFileNames: `[name].js?v=${version}`,
+				chunkFileNames: `[name].js?v=${version}`,
+				assetFileNames: `[name].[ext]?v=${version}`
+			}
 		},
-		sourcemap: false
-	  }
-	
+		sourcemap: false,
+		commonjsOptions: {
+			sourceMap: false,
+		},
+		minify: true,
+	}
+
 	/*  ,
 	ssr: {
 		noExternal: ["jsforce"], // add all tsparticles libraries here, they're not made for SSR, they're client only
 	  }
 	  */
-	
+
 });
